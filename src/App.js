f0be74fd7fca8dd096f7939fc0cebcf4ef51777c
maher <chrismaher.wit@gmail.com>
2019-07-08 15:48:30 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import FacebookLogin from 'react-facebook-login';
 
 function App() {
+
+    const responseFacebook = (response) => {
+        console.log(response);
+    }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+            <FacebookLogin
+                appId="2009920755997639" //APP ID NOT CREATED YET
+                fields="name,email,picture"
+                callback={responseFacebook}
+            />
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
   );
