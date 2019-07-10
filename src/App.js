892ea8889dda415c0fa37c0c1e7cb0808c744555
Chris Maher <chrismaher.wit@gmail.com>
2019-07-10 20:08:39 +0100

    Join the Sesh.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, {useState} from 'react';
-import React, { Component } from "react"
 import logo from './logo.svg';
 import './App.css';
 import FacebookLogin from 'react-facebook-login';
